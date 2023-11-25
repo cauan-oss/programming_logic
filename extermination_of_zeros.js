92dbf309a6ee9b1569067e813e8e5fceea17d651
Cauan <cauanbarros14@gmail.com>
2023-11-25 15:33:28 -0300

    discovering new things

--- extermination_of_zeros.js
+++ extermination_of_zeros.js
@@ -1,7 +1,9 @@
 // exercise one; get you primary two elements of input and take them ou in reverse ordem. Repeat untill the input is empty.
 
-const platform_one = [3, 6, "k", "m", 1, 6]
+const printing_objects = [{nome: "cauan", heigth: 1.80}]
 
-for(index = 0; index <= 4 ; index += 1) {
-    console.log(platform_one[index])
+const platform_input = [3, 6, "k", "m", 1, 6]
+
+for(index = 0; index <3; index += 1){
+    console.log(platform_input[index])
 }
